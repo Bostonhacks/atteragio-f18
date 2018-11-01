89d257d5ea6daa0b76978f77f360a39625c026ee
Bianca Oparaji <chinwebiancaoparaji@dhcp-wifi-8021x-155-41-65-58.bu.edu>
2018-11-01 19:20:38 -0400

    Minor Details gradient easing and spacing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <div id="app">
-    <b-navbar fixed="top" id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/faq">FAQ</router-link> |
-      <router-link to="/schedule">Schedule</router-link> |
-      <router-link to="/map">Map</router-link> |
-      <router-link to="/resources">Resources</router-link> |
-      <router-link to="/workshops">Workshops</router-link> |
-      <router-link to="/prizes">Prizes</router-link>
-    </b-navbar>
-    <router-view/>
+  <div id="app">    
+    <div class="navContain">
+      <b-navbar fixed="top" id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/faq">FAQ</router-link> |
+          <router-link to="/schedule">Schedule</router-link> |
+          <router-link to="/map">Map</router-link> |
+          <router-link to="/resources">Resources</router-link> |
+          <router-link to="/workshops">Workshops</router-link> |
+          <router-link to="/prizes">Prizes</router-link>
+      </b-navbar>
+      <router-view/>
+    </div>
+    
   </div>
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Roboto:400,700');
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,700');
 
 body,html{
-  padding-top: 60px;
+  padding-top: 9vw;
   width: 100%;
   height: 100vh;
-  background-color: #282561;
+  background-color:#a73d59;
 
   background-image: linear-gradient(
     to bottom,
@@ -35,11 +38,11 @@ body,html{
     hsl(323.01, 41.05%, 33.21%) 71%,
     hsl(331.84, 43.23%, 36.98%) 77.2%,
     hsl(338.8, 44.94%, 40.92%) 82.7%,
-    hsl(344.48, 46.3%, 45.03%) 87.6%,
+    hsl(344, 46%, 45%) 87.6%,
     hsl(349.23, 47.38%, 49.3%) 91.8%,
     hsl(353.29, 55.98%, 53.71%) 95.3%,
     hsl(356.85, 68.33%, 58.26%) 98%,
-    hsl(0, 84.13%, 62.94%) 100%
+    hsl(0, 84%, 63%) 100%
   );
 }
 
@@ -50,13 +53,14 @@ body,html{
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
+  font-size: 1.5vw;
 }
 
 
 
 #nav {
-  background-color: #7F779B;
+  background-color: #6b6691;
   width: 100%;
   height: 5vw;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <b-container class="bv-example-row">
+    <b-container> 
         <b-row class="homeHead">
             <b-col>Flair</b-col>
             <b-col cols="6" id="heroText">
               <div >
                 <p class="heroText"> Boston<span id="bolder">Hacks</span> 2018 </p>
-                <p>why is this doing stupid stuff</p>
+                <p class="date">November 10 &#8211 November 11,2018<br>
+                775 Commonwealth Avenue, Boston University</p>
               </div>
             </b-col>
             <b-col>Flair</b-col>
         </b-row>
     </b-container>
+    <b-container>
+      <b-row>
+        <b-col>Wifi</b-col>
+        <b-col>Slack</b-col>
+        <b-col>Devpost</b-col>
+      </b-row>
+    </b-container>
+
   </div>
   
 </template>
@@ -24,15 +33,20 @@ export default {
 </script>
 
 <style lang="scss">
+
+
 #heroText{
-  width: 45vh;
 }
 .heroText {
   color: white;
   font-family: "Roboto", Arial, Helvetica, sans-serif;
   font-weight: 400;
   font-size: 3.7vw;
-  display: inline;
+}
+
+.date{
+  margin-top: -1vh;
+  line-height: 2vw;
 }
 
 #bolder {
@@ -40,8 +54,12 @@ export default {
 }
 
 .homeHead{
-  padding-top: 10vw;
+  padding-top: 1vh;
 }
-
+@media (max-width: 600px) {
+  .homeHead {
+    margin-top: -5vw;
+  }
+}
 
 </style>
