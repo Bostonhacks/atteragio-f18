16d5bd1b0bf0dd40437a4c46baf8073fc95312e7
Bianca Oparaji <chinwebiancaoparaji@dhcp-wifi-8021x-155-41-65-58.bu.edu>
2018-11-01 15:19:40 -0400

    Gradient and HeroText Start Easing gradient for pages added--might change

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <b-navbar fixed="top" id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/faq">FAQ</router-link> |
       <router-link to="/schedule">Schedule</router-link> |
@@ -8,21 +8,59 @@
       <router-link to="/resources">Resources</router-link> |
       <router-link to="/workshops">Workshops</router-link> |
       <router-link to="/prizes">Prizes</router-link>
-    </div>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,700');
+
+body,html{
+  padding-top: 60px;
+  width: 100%;
+  height: 100vh;
+  background-color: #282561;
+
+  background-image: linear-gradient(
+    to bottom,
+    hsl(243, 45.45%, 25.88%) 0%,
+    hsl(244.69, 45.23%, 25.91%) 10.8%,
+    hsl(249.68, 44.51%, 26.01%) 21%,
+    hsl(257.48, 43.27%, 26.18%) 30.8%,
+    hsl(267.68, 41.51%, 26.42%) 40%,
+    hsl(280.2, 39.26%, 26.72%) 48.6%,
+    hsl(295.36, 36.56%, 27.09%) 56.7%,
+    hsl(311.31, 38.26%, 29.66%) 64.1%,
+    hsl(323.01, 41.05%, 33.21%) 71%,
+    hsl(331.84, 43.23%, 36.98%) 77.2%,
+    hsl(338.8, 44.94%, 40.92%) 82.7%,
+    hsl(344.48, 46.3%, 45.03%) 87.6%,
+    hsl(349.23, 47.38%, 49.3%) 91.8%,
+    hsl(353.29, 55.98%, 53.71%) 95.3%,
+    hsl(356.85, 68.33%, 58.26%) 98%,
+    hsl(0, 84.13%, 62.94%) 100%
+  );
+}
+
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  
+  font-family: "Roboto", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
+
+
+
 #nav {
-  padding: 30px;
+  background-color: #7F779B;
+  width: 100%;
+  height: 5vw;
+
+ 
   a {
     font-weight: bold;
     color: #2c3e50;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
-  <div style=" height: 10%; position:fixed;"class="home">
-    <p> this is the home page</p>
+  <div>
+    <b-container class="bv-example-row">
+        <b-row class="homeHead">
+            <b-col>Flair</b-col>
+            <b-col cols="6" id="heroText">
+              <div >
+                <p class="heroText"> Boston<span id="bolder">Hacks</span> 2018 </p>
+                <p>why is this doing stupid stuff</p>
+              </div>
+            </b-col>
+            <b-col>Flair</b-col>
+        </b-row>
+    </b-container>
   </div>
+  
 </template>
 
 <script>
@@ -12,8 +24,24 @@ export default {
 </script>
 
 <style lang="scss">
-  body {
-    height: 100vh;
-    background-image: linear-gradient(#282561, #F05352);
-  }
+#heroText{
+  width: 45vh;
+}
+.heroText {
+  color: white;
+  font-family: "Roboto", Arial, Helvetica, sans-serif;
+  font-weight: 400;
+  font-size: 3.7vw;
+  display: inline;
+}
+
+#bolder {
+  font-weight: 700;
+}
+
+.homeHead{
+  padding-top: 10vw;
+}
+
+
 </style>
