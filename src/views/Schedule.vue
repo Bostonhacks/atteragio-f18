e0433c42caf513dab9a7790ca45ac099a30284d5
nampham148 <phamquangnam148@gmail.com>
2018-11-04 15:12:16 -0500

    remove package-lock.json

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -89,43 +89,6 @@
       return {
         event_slot_height: 50, // = height of a time slot, hard coded for now
         unit_duration: 30, // unit duration is 30 mins
-        /*
-        timelines: [
-          "8:30","9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"
-        ],
-        events: [
-          [
-            {
-              start: "9:30",
-              end: "10:30",
-              type: "event-1",
-              full_name: "First event"
-            },
-            {
-              start: "11:30",
-              end: "12:30",
-              type: "event-1",
-              full_name: "Second event"
-            }
-          ],
-          [
-            {
-              start: "10:30",
-              end: "12:30",
-              type: "event-2",
-              full_name: "Third Event"
-            }
-          ],
-          [
-            {
-              start: "10:30",
-              end: "12:30",
-              type: "event-2",
-              full_name: "Fourth Event"
-            }
-          ]
-        ]
-        */
       }
     }
   }
