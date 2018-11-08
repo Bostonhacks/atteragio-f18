0f50822b5a80382f90eca9a90a92368fc900d49d
maridematte <marianag.dematte@gmail.com>
2018-11-08 10:38:40 -0500

    Did workshop page and removed resources from the available pages

--- src/views/Workshops.vue
+++ src/views/Workshops.vue
@@ -1,5 +1,68 @@
 <template>
-  <div class="map">
-    <h1>This is the workshops page</h1>
+  <div class="Workshops">
+    <Live />
+    <b-container fluid>
+      <b-row id="title">
+        <b-col sm="1" />
+        <b-col sm="10" md="10">
+          <p> Workshops </p>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col sm="0" md="1" />
+        <b-col>
+          <Card
+            v-for="card in cards"
+            :title="card.title"
+            :time = "card.time"
+            :room = "card.room"
+          />
+        </b-col>
+        <b-col sm="0" md="1" />
+      </b-row>
+      <Flair />
+    </b-container>
   </div>
 </template>
+
+
+<style scoped>
+
+#title{
+  margin-top: 10vh;
+  font-weight: 300;
+  font-size: 3rem;
+  padding-bottom: 6vh;
+}
+</style>
+
+<script>
+import Card from "../components/Card.vue"
+import Live from "../components/Live.vue"
+import Flair from "../components/Flair.vue"
+
+export default {
+  name: "Workshops",
+  components: {
+    Card,
+    Live,
+    Flair
+  },
+  data () {
+    return {
+      cards: [
+        {
+          title: "Intro to Web dev",
+          time: "2:00 - 3:00",
+          room: "Room 320",
+        },
+        {
+          title: "Unit 3D",
+          time: "2:00 - 3:00",
+          room: "Room 228"
+        }
+      ],
+    }
+  }
+}
+</script>
