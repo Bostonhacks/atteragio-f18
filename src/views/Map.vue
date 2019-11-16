c4badbb5ba21b8df3dcddaa18d263f38475951f4
mhendrick98 <mhendric@bu.edu>
2019-11-16 14:40:42 -0500

    wack surpise

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -32,99 +32,94 @@
 </template>
 
 <script>
-  import MapFloor from "./MapFloor.vue"
+import MapFloor from "./MapFloor.vue";
 
-  export default {
-    name: "Map",
-    components: {
-      MapFloor
-    },
-    data () {
-      return {
-        current_floor: 0,
-        maps: 
-        [
-          {
-            number: 0,
-            restrooms: false,
-            itemList:
-            [
-              {
-                numbering: "1",
-                title: "BU Central",
-                description: "Food, food, food"
-              },
-              {
-                numbering: "2",
-                title: "The Alley",
-                description: "More food"
-              }
-            ],
-            map_image: require("../assets/Maps/Floor0.svg")
-          },
-          {
-            number: 1,
-            restrooms: true,
-            itemList:
-            [
-              {
-                numbering: "1",
-                title: "GSU Student Link",
-                description: "Main check in"
-              },
-              {
-                numbering: "2",
-                title: "GSU Back Court",
-                description: "Team formation"
-              }
-            ],
-            map_image: require("../assets/Maps/Floor1.svg")
-          },
-          {
-            number: 2,
-            restrooms: true,
-            itemList:
-            [
-              {
-                numbering: "3",
-                title: "Metcalf Ballroom",
-                description: "Main hacking floor"
-              },
-              {
-                numbering: "6",
-                title: "Ziskind Lounge",
-                description: "Sponsors' Zone"
-              },
-              {
-                numbering: "7",
-                title: "Terrace Lounge",
-                description: "Surprise!"
-              }
-            ],
-            map_image: require("../assets/Maps/Floor2.svg")
-          },
-          {
-            number: 3,
-            restrooms: true,
-            itemList:
-            [
-              {
-                numbering: "4",
-                title: "GSU 3rd floor rooms",
-                description: "Breakdown rooms"
-              }
-            ],
-            map_image: require("../assets/Maps/Floor3.svg")
-          }
-        ]       
-      }
-    },
-    methods: {
-      changeFloor: function(number) {
-        this.current_floor = number;
-      }
+export default {
+  name: "Map",
+  components: {
+    MapFloor
+  },
+  data() {
+    return {
+      current_floor: 0,
+      maps: [
+        {
+          number: 0,
+          restrooms: false,
+          itemList: [
+            {
+              numbering: "1",
+              title: "BU Central",
+              description: "Food, food, food"
+            },
+            {
+              numbering: "2",
+              title: "The Alley",
+              description: "More food"
+            }
+          ],
+          map_image: require("../assets/Maps/Floor0.svg")
+        },
+        {
+          number: 1,
+          restrooms: true,
+          itemList: [
+            {
+              numbering: "1",
+              title: "GSU Student Link",
+              description: "Main check in"
+            },
+            {
+              numbering: "2",
+              title: "GSU Back Court",
+              description: "Team formation"
+            }
+          ],
+          map_image: require("../assets/Maps/Floor1.svg")
+        },
+        {
+          number: 2,
+          restrooms: true,
+          itemList: [
+            {
+              numbering: "3",
+              title: "Metcalf Ballroom",
+              description: "Main hacking floor"
+            },
+            {
+              numbering: "6",
+              title: "Ziskind Lounge",
+              description: "Sponsors' Zone"
+            },
+            {
+              numbering: "7",
+              title: "Terrace Lounge",
+              description: "Event Space!"
+            }
+          ],
+          map_image: require("../assets/Maps/Floor2.svg")
+        },
+        {
+          number: 3,
+          restrooms: true,
+          itemList: [
+            {
+              numbering: "4",
+              title: "GSU 3rd floor rooms",
+              description: "Breakdown rooms"
+            }
+          ],
+          map_image: require("../assets/Maps/Floor3.svg")
+        }
+      ]
+    };
+  },
+  methods: {
+    changeFloor: function(number) {
+      this.current_floor = number;
     }
   }
+};
 </script>
 
 <style scoped>
@@ -160,7 +155,7 @@
   position: relative;
   background-color: #928aa6;
   height: 8vw;
-  padding-left: 2vw;  
+  padding-left: 2vw;
 }
 
 .floor-tag {
@@ -196,8 +191,7 @@
 
 .map-body {
   margin-top: 4.5vw;
-  padding: 3vw 4vw  ;
+  padding: 3vw 4vw;
   border-top: 1px solid #b8b8b8;
 }
-
 </style>
